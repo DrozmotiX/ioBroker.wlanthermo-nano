2034c9807b53c43f5df936d6bf0d277b8082864a
Dutchman <rdrozda86@gmail.com>
2019-08-14 19:07:08 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'wlanthermo-nano adapter settings': {
+		'en': 'Adapter settings for wlanthermo-nano',
+		'de': 'Adaptereinstellungen für wlanthermo-nano',
+		'ru': 'Настройки адаптера для wlanthermo-nano',
+		'pt': 'Configurações do adaptador para wlanthermo-nano',
+		'nl': 'Adapterinstellingen voor wlanthermo-nano',
+		'fr': "Paramètres d'adaptateur pour wlanthermo-nano",
+		'it': "Impostazioni dell'adattatore per wlanthermo-nano",
+		'es': 'Ajustes del adaptador para wlanthermo-nano',
+		'pl': 'Ustawienia adaptera dla wlanthermo-nano',
+		'zh-cn': 'wlanthermo-nano的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
