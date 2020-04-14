801d891537d5ce53a11f0a613cf42d2aa284647d
DutchmanNL <rdrozda86@gmail.com>
2020-04-14 22:07:25 +0200

    remove Sentry test case

--- main.js
+++ main.js
@@ -36,7 +36,6 @@ class WlanthermoNano extends utils.Adapter {
 	async onReady() {
 
 		this.log.info('WLANThermo startet, loading devices');
-		setTimeout(sentryTest, 10000);
 		// Nasty workarround only test
 		initialise = true;
 
